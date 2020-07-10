4e180a310614338cb3d830b7b1d489c7aa3999c9
HarithaRugada <haritharugada@gmail.com>
2020-07-10 17:44:43 +0530

    ADDED : Step - 1 Return Total Fare

--- src/test/java/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -1,5 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CabInvoiceGeneratorTest {
 
-
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
 }
