bf6804df252c038cc0ca042308fea1dc4df083a3
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-10 18:21:36 +0530

    ADDED : Test case for multiple rides

--- src/test/java/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -21,4 +21,17 @@ public class CabInvoiceGeneratorTest {
         double fare = cabInvoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
     }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        CabInvoiceGenerator cabInvoiceGenerator =  new CabInvoiceGenerator();
+        Ride[] rides = { new Ride(2.0,5),
+                        new Ride(0.1,1)
+                        };
+        double fare = cabInvoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30,fare,0.0);
+
+
+
+    }
 }
